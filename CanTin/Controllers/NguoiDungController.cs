14202dca83069f6596f80e9ca1f3e00780a36bbb
pc <pc@DESKTOP-35187N6>
2023-04-03 14:16:16 +0700

    Add project files.

---
+++ CanTin/Controllers/NguoiDungController.cs
@@ -0,0 +1,80 @@
+﻿using CanTin.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CanTin.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        MyDataDataContext data= new MyDataDataContext();
+        [HttpGet]
+        // GET: NguoiDung
+        public ActionResult Dangky()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Dangky (FormCollection collection, KhachHang kh)
+        {
+            var hoten = collection["hoten"];
+            var tendangnhap = collection["tendangnhap"];
+            var matkhau = collection["matkhau"];
+            var matkhauxacnhan = collection["matkhauxacnhan"];
+            var email = collection["email"];
+            var diachi = collection["diachi"];
+            var dienthoai = collection["dienthoai"];
+            var ngaysinh = String.Format("{0:MM/dd/yyyy}", collection["ngaysinh"]);
+            if (String.IsNullOrEmpty(matkhauxacnhan) ) 
+            {
+                ViewData["NhapMKXN"] = "Phải nhập mật khẩu xác nhận! ";
+            }
+            else
+            {
+                if(!matkhau.Equals(matkhauxacnhan))
+                {
+                    ViewData["MatKhauGiongNhau"] = "Mật khẩu và mật khẩu xác nhận phải giống nhau";
+                }    
+                else
+                {
+                    kh.hoten = hoten;
+                    kh.tendangnhap = tendangnhap;
+                    kh.matkhau= matkhau;
+                    kh.email= email;
+                    kh.diachi = diachi;
+                    kh.dienthoai = dienthoai;
+                    kh.ngaysinh = DateTime.Parse(ngaysinh);
+                    data.KhachHangs.InsertOnSubmit(kh);
+                    data.SubmitChanges();
+                    return RedirectToAction("Dangnhap");
+                }    
+            }
+            return this.Dangky();
+        }
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection collection)
+        {
+            var tendangnhap = collection["tendangnhap"];
+            var matkhau = collection["matkhau"];
+            KhachHang kh = data.KhachHangs.SingleOrDefault(n => n.tendangnhap == tendangnhap && n.matkhau == matkhau);
+            if (kh != null)
+            {
+                ViewBag.ThongBao = "Chúc mừng đăng nhập thành công";
+                Session["TaiKhoan"] = kh;
+            }
+            else
+            {
+                ViewBag.ThongBao = "Tên đăng nhập hoặc mật khẩu không đúng";
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ CanTin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CanTin.MvcApplication" Language="C#" %>
